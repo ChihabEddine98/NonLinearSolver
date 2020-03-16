de7245e8ae98d81838ad9e75d7578c558a3a7ed0
abenam27 <ga_benamara@esi.dz>
2020-03-16 13:35:43 +0100

    fix test 1 for dichotomie method

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ from tests import *
 
 
 if __name__ == '__main__':
-    print("-----------------------------------")
-    print("|        Méthode Dichotomie        |")
-    print("-----------------------------------")
-    f1_str=Tests_Dico.affiche_f1()
-    print(f" Fonction   : {f1_str}")
-    interval =[0,1]
-    print(f" Intervalle : {interval}")
-    err=10**-8
-    print(f" Erreur     : {err} \n")
-    sol=dct.res_equa_dct(Tests_Dico.f1,interval[0],interval[1],err)
-    print(f" Solution approchée de f1(x) = 0 est : {sol}\n")
+    # Tests Méthode Dichotomie :
+    test1()
     
     
--- tests.py
+++ tests.py
@@ -1,9 +1,24 @@
-
+import dichotomie as dct
 
 class Tests_Dico():
 
     # f1(x) = x^3+x-1
-    def affiche_f1():
+    def affiche_f1(self):
         return "f1(x) = x^3+x-1 "
-    def f1(x):
+    def f1(self,x):
         return x**3+x-1 
+
+def test(f,f_str,interval,err):
+    tst_dico=Tests_Dico()
+    print("-----------------------------------")
+    print("|        Méthode Dichotomie        |")
+    print("-----------------------------------")
+    print(f" Fonction   : {f_str}")
+    print(f" Intervalle : {interval}")
+    print(f" Erreur     : {err} \n")
+    sol=dct.res_equa_dct(f,interval[0],interval[1],err)
+    print(f" Solution approchée de f1(x) = 0 est : {sol}\n")
+
+def test1():
+    tst_dico=Tests_Dico()
+    test(tst_dico.f1,tst_dico.affiche_f1(),[0,1],10**-8)

83588f40f51bd3e03a776397a0fd1ab30d04cfa8
abenam27 <ga_benamara@esi.dz>
2020-03-16 13:22:37 +0100

    test dichotomie

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import dichotomie as dct
-
+from tests import *
 
 
 
 if __name__ == '__main__':
-    dct.res_equa_dct(f,1,2,0.001)
+    print("-----------------------------------")
+    print("|        Méthode Dichotomie        |")
+    print("-----------------------------------")
+    f1_str=Tests_Dico.affiche_f1()
+    print(f" Fonction   : {f1_str}")
+    interval =[0,1]
+    print(f" Intervalle : {interval}")
+    err=10**-8
+    print(f" Erreur     : {err} \n")
+    sol=dct.res_equa_dct(Tests_Dico.f1,interval[0],interval[1],err)
+    print(f" Solution approchée de f1(x) = 0 est : {sol}\n")
+    
     
---
+++ tests.py
@@ -0,0 +1,9 @@
+
+
+class Tests_Dico():
+
+    # f1(x) = x^3+x-1
+    def affiche_f1():
+        return "f1(x) = x^3+x-1 "
+    def f1(x):
+        return x**3+x-1 

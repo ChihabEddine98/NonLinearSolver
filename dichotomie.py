463f1cb5ef56e03e83aed4eba7f113757d7f29b0
abenam27 <ga_benamara@esi.dz>
2020-03-16 12:35:43 +0100

    dichotomie mais pas encore implementée

---
+++ dichotomie.py
@@ -0,0 +1,18 @@
+
+
+
+
+"""
+    res_equa_dct(f,a,b)
+
+    Cette fonction résout l'equation f(x)=0 
+    pour une fonction f monotonne (croissante ou décroissante)
+    sur un intervale [a,b] le pseudo code de cette fonction
+    est disponible sur le ReadMe du dépot  
+"""
+def res_equa_dct(f,a,b):
+    return 0
+
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import dichotomie as dct
+
+
+
+
+if __name__ == '__main__':
+    dct.res_equa_dct(f,1,2)
+    

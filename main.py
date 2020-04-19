2d18827fd6d148f81f5f55654df38bb3800a1f1e
abenam27 <ga_benamara@esi.dz>
2020-04-19 18:30:04 +0200

    work in clean version

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from tests.Tester import Tester
 from gui.App import *
-<<<<<<< HEAD
-import sys
-from PyQt5 import QtWidgets
-=======
->>>>>>> 95d7be8ce68a628b1c5648230ca6268ec23c2045
+
 
 
 
@@ -15,13 +11,10 @@ if __name__ == '__main__':
 
     # Tests Des méthodes :
 
-<<<<<<< HEAD
+
     guiApp=Application()
     guiApp.mainloop()
     tester=Tester()
     tester.test()
-=======
-    #tester=Tester()
-    #tester.test()
->>>>>>> 95d7be8ce68a628b1c5648230ca6268ec23c2045
+
     

6a45c4c30bccb1de8be8ef0cd3a1102924c264b0
ybenic28 <benichouyanivpro@gmail.com>
2020-05-11 10:52:27 +0200

    modularité du code, calcul de convergence

--- tests/FalsePositionTester.py
+++ tests/FalsePositionTester.py
@@ -2,87 +2,66 @@ from algos.FalsePosition import *
 from algos.Equa_Solver import *
 from convergence_rate import *
 
-
-
-class FalsePositionTester :
+class FalsePositionTester:
 
     def test1(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "x**2-cos(x)"
-        equa=Equa_Solver(f=f,err=1e-8,a=0.0,b=1.0)
-        x_list=FalsePosition.solve(equa)
+        equa = Equa_Solver(f=f, err=1e-8, a=0.0, b=1.0)
+        x_list = FalsePosition.solve(equa)
 
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
+        print_rate(x_list)
 
     def test2(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "x**3 - x**2 - 1"
-        equa=Equa_Solver(f=f,err=1e-8,a=1.0,b=2.0)
-        x_list=FalsePosition.solve(equa)
+        equa = Equa_Solver(f=f, err=1e-8, a=1.0, b=2.0)
+        x_list = FalsePosition.solve(equa)
 
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
+        print_rate(x_list)
 
     def test3(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "cos(2*x)**2 - x**2"
-        equa=Equa_Solver(f=f,a=0.0,b=1)
-        x_list=FalsePosition.solve(equa)
+        equa = Equa_Solver(f=f, a=0.0, b=1)
+        x_list = FalsePosition.solve(equa)
 
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
+        print_rate(x_list)
 
     def test4(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "cos(x)"
-        equa=Equa_Solver(f=f,a=0.0,b=3.0)
-        x_list=FalsePosition.solve(equa)
+        equa = Equa_Solver(f=f, a=0.0, b=3.0)
+        x_list = FalsePosition.solve(equa)
 
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
+        print_rate(x_list)
 
     def test5(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "x**3 - 4*x + 1"
-        equa=Equa_Solver(f=f,a=-3.0,b=-2.0)
-        x_list=FalsePosition.solve(equa)
-        
-        x_final=x_list[-1]
-        x_list.pop()
+        equa = Equa_Solver(f=f, a=-3.0, b=-2.0)
+        x_list = FalsePosition.solve(equa)
 
-        print_rates(x_list,x_final)
+        print_rate(x_list)
 
     def test6(self):
         print("-----------------------------------")
         print("|        Méthode Fausse position   |")
         print("-----------------------------------", )
         f = "x-exp(-x)"
-        equa=Equa_Solver(f=f,a=0.0,b=1.0)
-        x_list=FalsePosition.solve(equa)
-
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
+        equa = Equa_Solver(f=f, a=0.0, b=1.0)
+        x_list = FalsePosition.solve(equa)
+        print_rate(x_list)
 
     def test(self):
         self.test1()
--- tests/NewtonTester.py
+++ tests/NewtonTester.py
@@ -4,80 +4,60 @@ from convergence_rate import *
 
 import math
 
+
 class NewtonTester:
 
     def test1(self):
         print("-----------------------------------")
         print("|        Méthode Newton            |")
         print("-----------------------------------", )
-        f="x**2-cos(x)"
-        Df="2*x+sin(x)"
-        equa=Newton(f=f,df=Df,err=1e-8,x0=math.pi/4,max_iter=10)
-        x_list=Newton.solve(equa)
+        f = "x**2-cos(x)"
+        Df = "2*x+sin(x)"
+        equa = Newton(f=f, df=Df, err=1e-8, x0=math.pi/4, max_iter=10)
+        x_list = Newton.solve(equa)
 
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
-    
+        print_rate(x_list)
 
     def test2(self):
         print("-----------------------------------")
         print("|        Méthode Newton            |")
         print("-----------------------------------", )
 
-        f="x**3 - x**2 - 1"
-        Df="3*x**2-2*x"
-        equa=Newton(f=f,df=Df,err=1e-8,x0=0.75,max_iter=10)
-        x_list=Newton.solve(equa)
-        
-        x_final=x_list[-1]
-        x_list.pop()
+        f = "x**3 - x**2 - 1"
+        Df = "3*x**2-2*x"
+        equa = Newton(f=f, df=Df, err=1e-8, x0=0.75, max_iter=10)
+        x_list = Newton.solve(equa)
 
-        print_rates(x_list,x_final)
-    
+        print_rate(x_list)
+
     def test3(self):
         print("-----------------------------------")
         print("|        Méthode Newton            |")
         print("-----------------------------------", )
-        f="cos(2*x)**2 - x**2"
-        equa=Newton(f=f,err=1e-10,x0=0.75,max_iter=10)
-        x_list=Newton.solve(equa)
-        print("X_LIST : ",x_list)
-        x_final=x_list[-1]
-        x_list.pop()
-
-        print_rates(x_list,x_final)
-    
+        f = "cos(2*x)**2 - x**2"
+        equa = Newton(f=f, err=1e-10, x0=0.75, max_iter=10)
+        x_list = Newton.solve(equa)
+        print_rate(x_list)
 
     def test4(self):
         print("-----------------------------------")
         print("|        Méthode Newton            |")
         print("-----------------------------------", )
-        f="cos(x)"
-        equa=Newton(f=f,err=1e-10,x0=1.0,max_iter=10)
-        x_list=Newton.solve(equa)
-        
-        x_final=x_list[-1]
-        x_list.pop()
+        f = "cos(x)"
+        equa = Newton(f=f, err=1e-10, x0=1.0, max_iter=10)
+        x_list = Newton.solve(equa)
 
-        print_rates(x_list,x_final)
-    
+        print_rate(x_list)
 
     def test5(self):
         print("-----------------------------------")
         print("|        Méthode Newton            |")
         print("-----------------------------------", )
-        f="x**3 - 4*x + 1"
-        equa=Newton(f=f,err=1e-10,x0=-3,max_iter=10)
-        x_list=Newton.solve(equa)
-        
-        x_final=x_list[-1]
-        x_list.pop()
+        f = "x**3 - 4*x + 1"
+        equa = Newton(f=f, err=1e-10, x0=-3, max_iter=10)
+        x_list = Newton.solve(equa)
 
-        print_rates(x_list,x_final)
-    
-
+        print_rate(x_list)
 
     def test(self):
         self.test1()
@@ -85,4 +65,3 @@ class NewtonTester:
         self.test3()
         self.test4()
         self.test5()
-
